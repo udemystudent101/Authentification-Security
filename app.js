47ea7b72c2125518592c9466ad99473ecba85338
Léo <leo_24@live.fr>
2022-12-17 21:35:17 +0100

    security_level__3_hashing_md5

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5")
 
  
 const app = express();
@@ -29,9 +29,7 @@ const userSchema = new mongoose.Schema({
 
 console.log(process.env.API_KEY);
 
-// MONGOOSE-ENCRYPTION
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:["password"]});
 
 
 
@@ -56,7 +54,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if (err) {
@@ -69,7 +67,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser){
         if (err){
